96ae2681c5ab59f0aa5b1420f731deecf66b6bb4
Sergey <sergey.cherenkov96@gmail.com>
2023-04-18 19:42:57 +0300

    optimize slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,7 +3,9 @@ import { Navigation, Autoplay } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay'
-
+import { SliderStyle } from './Slider.styled';
+import SliderPrevButton from './SliderPrevButton';
+import SliderNextButton from './SliderNextButton';
 
 
 import slide0 from '../../images/img/slider-img-0-min.jpg'
@@ -11,9 +13,9 @@ import slide1 from '../../images/img/slider-img-1-min.jpg'
 import slide2 from '../../images/img/slider-img-2-min.jpg'
 import slide3 from '../../images/img/slider-img-3-min.jpg'
 import slide4 from '../../images/img/slider-img-4-min.jpg'
-import { SliderStyle } from './Slider.styled';
-import SliderPrevButton from './SliderPrevButton';
-import SliderNextButton from './SliderNextButton';
+
+const slidesArr = [slide0, slide1, slide2, slide3, slide4, slide0, slide1, slide2, slide3, slide4];
+
 
 
 const Slider = () => {
@@ -44,17 +46,7 @@ const Slider = () => {
                         spaceBetween: 42,
                     }
                 }}
-            >
-                <SwiperSlide><img src={slide0} alt="slider" /></SwiperSlide>
-                <SwiperSlide><img src={slide1} alt="slider" /></SwiperSlide>
-                <SwiperSlide><img src={slide2} alt="slider" /></SwiperSlide>
-                <SwiperSlide><img src={slide3} alt="slider" /></SwiperSlide>
-                <SwiperSlide><img src={slide4} alt="slider" /></SwiperSlide>
-                <SwiperSlide><img src={slide0} alt="slider" /></SwiperSlide>
-                <SwiperSlide><img src={slide1} alt="slider" /></SwiperSlide>
-                <SwiperSlide><img src={slide2} alt="slider" /></SwiperSlide>
-                <SwiperSlide><img src={slide3} alt="slider" /></SwiperSlide>
-                <SwiperSlide><img src={slide4} alt="slider" /></SwiperSlide>
+            > {slidesArr.map(slide => <SwiperSlide><img src={slide} alt="slider" /></SwiperSlide>)}
             <SliderPrevButton />
             <SliderNextButton />
             </Swiper>
